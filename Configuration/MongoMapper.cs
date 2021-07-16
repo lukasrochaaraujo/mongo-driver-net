3fe8cefcc1cdf3ea835199df30cba32447048ce9
Jorge Lucas <lukasrochaaraujo@hotmail.com>
2021-07-16 16:59:36 -0300

    Project commit

---
+++ Configuration/MongoMapper.cs
@@ -0,0 +1,27 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace MongoNetDriver
+{
+    public static class MongoMapper
+    {
+        public static void ApplyMapping()
+        {
+            //HomePeople
+            BsonClassMap.RegisterClassMap<HomePeople>(cm => 
+            {
+                cm.AutoMap();
+                cm.SetIgnoreExtraElements(true);
+                cm.MapIdMember(f => f.Id)
+                    .SetIdGenerator(StringObjectIdGenerator.Instance)
+                    .SetSerializer(new StringSerializer(BsonType.ObjectId));
+                cm.MapMember(f => f.Age)
+                    .SetElementName("age");
+                cm.MapMember(f => f.Name)
+                    .SetElementName("name");
+            });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace MongoNetDriver
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            MongoMapper.ApplyMapping();
+
+            var mongoClient = new MongoClient("mongodb://localhost:27017");
+            var database = mongoClient.GetDatabase("people");
+            var collection = database.GetCollection<HomePeople>("home");
+
+            //list all home people
+            var majorityAgeList = await collection.Find(p => p.Age >= 18).ToListAsync();
+            majorityAgeList.ForEach(people => Console.WriteLine(people));
+
+            //create
+            HomePeople newHomePeople = new("Fulano", 17);
+            await collection.InsertOneAsync(newHomePeople);
+            newHomePeople = await collection.Find(p => p.Name.Equals("Fulano")).FirstAsync();
+            Console.WriteLine(newHomePeople);
+
+            //filter builder
+            var filterBuilder = Builders<HomePeople>.Filter;
+
+            //filter to find
+            var filterToFind = filterBuilder.Eq("name", newHomePeople.Name) & filterBuilder.Eq("age", newHomePeople.Age);
+
+            //filter and update
+            newHomePeople.Name = "Sicrano";
+            var updatedInfo = Builders<HomePeople>
+                                .Update.Set("name", newHomePeople.Name)
+                                       .Set("age", newHomePeople.Age);
+
+            await collection.FindOneAndUpdateAsync(filterToFind, updatedInfo);
+            Console.WriteLine(newHomePeople);
+
+            //filter and delete
+            var findFilterToDelete = filterBuilder.Eq("name", newHomePeople.Name) & filterBuilder.Eq("age", newHomePeople.Age);         
+            await collection.FindOneAndDeleteAsync(findFilterToDelete);
+            Console.WriteLine($"{newHomePeople.Name} was deleted from collection.");
+        }
+    }
+
+    class HomePeople
+    {
+        public HomePeople(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+            => $"Id={Id},Name={Name},Age={Age}";
+    }
+}
